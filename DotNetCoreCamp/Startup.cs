b45c3d0929c333de56962cf45565dc7f11ec5dbf
Berkcan Gümüşışık <gumusisikberkcan@gmail.com>
2021-11-23 23:24:16 +0300

    Admin paneline başlandı.

--- DotNetCoreCamp/Startup.cs
+++ DotNetCoreCamp/Startup.cs
@@ -74,8 +74,14 @@ namespace DotNetCoreCamp
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                name: "areas",
+                pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+              );
+
+
+                endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
             });
 
         }
