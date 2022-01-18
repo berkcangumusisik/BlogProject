e2ca4a4c02c9c37846fc42861ea6ce1ba028e96a
Berkcan Gümüşışık <gumusisikberkcan@gmail.com>
2022-01-18 20:27:54 +0300

    Api işlemlerine devam edildi.

--- BlogApiDemo/Controllers/DefaultController.cs
+++ BlogApiDemo/Controllers/DefaultController.cs
@@ -23,9 +23,63 @@ namespace BlogApiDemo.Controllers
         }
 
         [HttpPost]
-        public IActionResult EmployeeAdd()
+        public IActionResult EmployeeAdd(Employee employee)
         {
+            using var c = new Context();
+            c.Add(employee);
+            c.SaveChanges();
             return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult EmployeeGet(int id)
+        {
+            using var c = new Context();
+            var employee = c.Employees.Find(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(employee);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult EmployeeDelete(int id)
+        {
+            using var c = new Context();
+            var employee = c.Employees.Find(id);
+            if (employee == null)
+            {
+                return NotFound();
+
+            }
+            else
+            {
+                c.Remove(employee);
+                c.SaveChanges();
+                return Ok();
+            }
+        }
+
+        [HttpPut]
+        public IActionResult EmployeeUpdate(Employee employee)
+        {
+            using var c = new Context();
+            var emp = c.Find<Employee>(employee.ID);
+            if (emp == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                emp.Name = employee.Name;
+                c.Update(emp);
+                c.SaveChanges();
+                return Ok();
+            }
         }
     }
 }
